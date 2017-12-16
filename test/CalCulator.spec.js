034ba5199adef1e0d2ed4e4a2612e518b51975a5
Mostafa Kamal <radionishi@gmail.com>
2017-12-16 18:23:27 +0600

    karma

--- test/CalCulator.spec.js
+++ test/CalCulator.spec.js
@@ -1,4 +1,20 @@
+class Calculator{
+  add(x, y){
+    return x+y;
+  }
+  sub(x, y){
+    return x-y;
+  }
+}
 describe('this is describe', function () {
+  var calculate = new Calculator();
+
+  it('addition', function () {
+    expect(calculate.add(3, 4)).toEqual(7);
+  });
+  it('subtraction', function () {
+    expect(calculate.sub(13, 4)).toEqual(9);
+  });
   it('this is it', function () {
     console.log('ok done');
   });
