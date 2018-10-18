3bb7d20b4921590cb39efee6da9c0dbddb92aea7
Mostafa Kamal <radionishi@gmail.com>
2018-10-18 14:47:42 +0600

    require issue solve

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine','browserify'],
 
 
     // list of files / patterns to load in the browser
@@ -28,7 +28,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.js': ['coverage']
+      'src/**/*.js': ['coverage'],
+      'test/**/*.[sS]pec.js': ['browserify']
     },
 
 
